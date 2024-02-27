8e31335acc31936091d85598280db57a56635ddc
Heng123456830 <149472091+Heng123456830@users.noreply.github.com>
2024-02-27 09:59:24 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -63,29 +63,29 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         numberCount1Element.textContent = "";
         number1Element.textContent = "愿妳此生一生被爱，坦荡自若，"; // Display "生日快乐"
-    }, 12000);
+    }, 18000);
 
     // Function to display "happy birthday" after another 6 seconds
     setTimeout(function () {
         number2Element.textContent = "无忧无疾。祝你的生日充满美好"; // Display "happy birthday"
-    }, 15000); // 3 seconds after the greeting changes
+    }, 21000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number3Element.textContent = "的回忆，愿未来的日子更加辉煌。"; // Display "生日快乐"
-    }, 18000);
+    }, 24000);
 
     setTimeout(function () {
         number4Element.textContent = "愿你的生日充满欢笑和美好的回忆。"; // Display "happy birthday"
-    }, 21000); // 3 seconds after the greeting changes
+    }, 27000); // 3 seconds after the greeting changes
 
 
     setTimeout(function () {
         number5Element.textContent = "愿你的生日充满阳光、欢笑和温馨，"; // Display "happy birthday"
-    }, 24000); // 3 seconds after the greeting changes
+    }, 30000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number6Element.textContent = "永远快乐。妳若不伤，岁月无恙。"; // Display "happy birthday"
-    }, 27000); // 3 seconds after the greeting changes
+    }, 33000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number1Element.textContent = ""; // Display "happy birthday"
@@ -95,33 +95,33 @@ document.addEventListener("DOMContentLoaded", function () {
         number5Element.textContent = "";
         number6Element.textContent = "";
         
-    }, 27500); // 3 seconds after the greeting changes
+    }, 33500); // 3 seconds after the greeting changes
 
 
 
     setTimeout(function () {
         number7Element.textContent = "一場相遇，來自于緣分，"; // Display "happy birthday"
-    }, 28000); // 3 seconds after the greeting changes
+    }, 36000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number8Element.textContent = "一個真情，來自于誠意，"; // Display "happy birthday"
-    }, 31000); // 3 seconds after the greeting changes
+    }, 39000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number9Element.textContent = "一句問候，來自于思念，"; // Display "happy birthday"
-    }, 34000); // 3 seconds after the greeting changes
+    }, 42000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number10Element.textContent = "一份陪伴，給予力量，"; // Display "happy birthday"
-    }, 37000); // 3 seconds after the greeting changes
+    }, 45000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number11Element.textContent = "一份心動，來自於本能。"; // Display "happy birthday"
-    }, 40000); // 3 seconds after the greeting changes
+    }, 48000); // 3 seconds after the greeting changes
 
     setTimeout(function () {
         number12Element.textContent = "點擊"; // Display "happy birthday"
-    }, 43000); // 3 seconds after the greeting changes
+    }, 51000); // 3 seconds after the greeting changes
 });
 
 
